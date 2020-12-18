5b3091c6b74a8faf28011f55b1a90050829f4844
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-18 22:10:23 +0700

    Remove unused navbar menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -23,19 +23,6 @@ export default function Navbar() {
             Something
           </a>
         </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
     </nav>
   );
