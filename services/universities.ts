f01e16be3f0e3ff5436bd1d50f71676d6fea6286
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-18 21:54:21 +0700

    Implenent University interface on university service

--- services/universities.ts
+++ services/universities.ts
@@ -1,8 +1,14 @@
+import { AxiosResponse } from 'axios';
+import University from '../interfaces/university';
 import APIInstance from './api-instance';
 
 const API_PATH = '/universities';
 
-export const getUniversities = () => APIInstance.get(`${API_PATH}`);
+export const getUniversities =
+  (params = {}): Promise<University[]> =>
+    APIInstance.get(`${API_PATH}`, {
+      params,
+    }).then((response: AxiosResponse<University[]>) => response.data);
 
 export default {
   getUniversities,
