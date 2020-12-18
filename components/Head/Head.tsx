45ff304e7b3dcb1a8ef3c177566927ebcc5615fb
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-18 21:55:51 +0700

    Add component: Head

---
+++ components/Head/Head.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const DEFAULT_TITLE = 'Eka Setya Nugraha - xndt-tst';
+
+interface Props {
+  title?: string;
+}
+
+export default function ({
+  title = DEFAULT_TITLE,
+}: Props) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" integrity="sha512-ZRv40llEogRmoWgZwnsqke3HNzJ0kiI0+pcMgiz2bxO6Ew1DVBtWjVn0qjrXdT3+u+pSN36gLgmJiiQ3cQtyzA==" crossOrigin="anonymous" />
+    </Head>
+  )
+}
