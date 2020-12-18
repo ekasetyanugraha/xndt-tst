479e1973f913bab140eda1219ceb84340a2ec073
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-18 19:17:19 +0700

    Add component: Navbar

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar box m-0" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div className="navbar-menu container">
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Home
+          </a>
+
+          <a className="navbar-item">
+            Documentation
+          </a>
+
+          <a className="navbar-item">
+            Something
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
