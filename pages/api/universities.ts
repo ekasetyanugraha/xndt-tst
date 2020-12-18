d8dd15d768252a17e32b1af0d6f7df61f36b5da3
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-18 21:55:20 +0700

    Add filtering capability to University API

--- pages/api/universities.ts
+++ pages/api/universities.ts
@@ -1,9 +1,30 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import data from '../../data/world_universities_and_domain.json';
+import University from '../../interfaces/university';
+
+const handleGet = (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req;
+  const {
+    limit = 10,
+    offset = 0,
+    country = '',
+    name = '',
+  } = query;
+  const numLimit = Number(limit);
+  const numOffset = Number(offset);
+
+  const pageData: University[] =
+    data
+      .filter(university => country ? university.alpha_two_code === country : true)
+      .filter(university => university.name.includes(name))
+      .slice(numOffset, numLimit + numOffset);
+
+  res.status(200).json(pageData);
+};
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.json(data);
+  switch (req.method) {
+    case 'GET': handleGet(req, res);
+    default: handleGet(req, res);
   }
 }
