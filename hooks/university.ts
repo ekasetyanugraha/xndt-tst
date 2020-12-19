1c5c6117268eeb99a4a817a19c065b4c3cd6d358
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-19 13:29:21 +0700

    Adjust university hooks

--- hooks/university.ts
+++ hooks/university.ts
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { ApiResponseMetaPaginated } from 'interfaces/api-response';
+import University from 'interfaces/university';
 import UrlParamsUniversity from 'interfaces/url-params-university';
 import UniversityService from 'services/universities';
 
 export default function useUniversity () {
   const [params, setParams] = useState({} as UrlParamsUniversity);
-  const [meta, setMeta] = useState({});
+  const [meta, setMeta] = useState({} as ApiResponseMetaPaginated);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [universities, setUniversities] = useState([]);
+  const [universities, setUniversities] = useState([] as University[]);
 
   const getUniversities = async (params: UrlParamsUniversity = {}) => {
     setParams(params);
