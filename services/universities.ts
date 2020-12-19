897ec7f6448895f79d09ce33aff04f5515ec4ca7
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-19 13:29:12 +0700

    Adjust university service

--- services/universities.ts
+++ services/universities.ts
@@ -1,15 +1,15 @@
 import { AxiosResponse } from 'axios';
-import University from 'interfaces/university';
+import { ResponseGetUniversities } from 'interfaces/api-response-get-universities';
 import UrlParamsUniversity from 'interfaces/url-params-university';
 import APIInstance from './api-instance';
 
 const API_PATH = '/universities';
 
 export const getUniversities =
-  (params: UrlParamsUniversity = {}): Promise<University[]> =>
+  (params: UrlParamsUniversity = {}): Promise<ResponseGetUniversities> =>
     APIInstance.get(`${API_PATH}`, {
       params,
-    }).then((response: AxiosResponse<University[]>) => response.data);
+    }).then((response: AxiosResponse<ResponseGetUniversities>) => response.data);
 
 export default {
   getUniversities,
