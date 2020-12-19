8753c17d55bad9750f00ffc9782fc81f4d40592f
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-19 20:50:29 +0700

    Add session service

---
+++ services/session.ts
@@ -0,0 +1,11 @@
+import APIInstance from './api-instance';
+
+const API_PATH = '/sessions';
+
+export const login =
+  (data = {}) =>
+    APIInstance.post(`${API_PATH}`, data);
+
+export default {
+  login,
+};
