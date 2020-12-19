a085cdbfc5382b335c461b6e7b661958bce3458a
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-19 12:53:15 +0700

    Add meta to university hooks

--- hooks/university.ts
+++ hooks/university.ts
@@ -3,17 +3,21 @@ import UrlParamsUniversity from 'interfaces/url-params-university';
 import UniversityService from 'services/universities';
 
 export default function useUniversity () {
+  const [params, setParams] = useState({} as UrlParamsUniversity);
+  const [meta, setMeta] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [universities, setUniversities] = useState([]);
 
   const getUniversities = async (params: UrlParamsUniversity = {}) => {
+    setParams(params);
     setIsLoading(true);
     setIsError(false);
 
     try {
-      const universities = await UniversityService.getUniversities(params);
-      setUniversities(universities);
+      const { data, meta } = await UniversityService.getUniversities(params);
+      setUniversities(data);
+      setMeta(meta);
     } catch {
       setIsError(true);
     } finally {
@@ -22,11 +26,13 @@ export default function useUniversity () {
   };
 
   return {
+    params,
     isLoading,
     setIsLoading,
     isError,
     setIsError,
     universities,
+    meta,
     setUniversities,
     getUniversities,
   };
