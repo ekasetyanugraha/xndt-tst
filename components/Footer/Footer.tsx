fb24510c454650c63a32d806843a999a892349ba
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-20 15:40:09 +0700

    Add unit test: Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Footer(): JSX.Element {
-  return <footer className="footer"></footer>;
+  return <footer className="footer" data-testid="footer"></footer>;
 }
