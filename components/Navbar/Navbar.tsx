9a3141b2c13215f1b34d222c317d74fd9db280da
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-20 20:23:29 +0700

    Implement logout function on Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,21 @@
-import { User } from 'interfaces/user';
 import React from 'react';
+import { useRouter } from 'next/router';
+import { User } from 'interfaces/user';
+import sessionService from 'services/session';
 
 interface Props {
   user?: User;
 }
 
 export default function Navbar({ user }: Props): JSX.Element {
+  const router = useRouter();
+
+  const onClickLogout = async () => {
+    await sessionService.logout();
+
+    return router.push('/');
+  };
+
   return (
     <nav className="navbar box m-0" role="navigation" aria-label="main navigation">
       <div className="navbar-menu container">
@@ -20,7 +30,9 @@ export default function Navbar({ user }: Props): JSX.Element {
             <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link">{user.email}</a>
               <div className="navbar-dropdown is-right">
-                <a className="navbar-item">Logout</a>
+                <a className="navbar-item" onClick={onClickLogout}>
+                  Logout
+                </a>
               </div>
             </div>
           </div>
