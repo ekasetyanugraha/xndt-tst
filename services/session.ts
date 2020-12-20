86df37d5c6d496a2622d0c25426504efba684398
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-20 19:24:42 +0700

    Fix session service return type

--- services/session.ts
+++ services/session.ts
@@ -1,8 +1,9 @@
+import { AxiosResponse } from 'axios';
 import APIInstance from './api-instance';
 
 const API_PATH = '/sessions';
 
-export const login = (data = {}): Promise<void> => APIInstance.post(`${API_PATH}`, data);
+export const login = (data = {}): Promise<AxiosResponse> => APIInstance.post(`${API_PATH}`, data);
 
 export default {
   login,
